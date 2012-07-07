472440950d9f3ceb1ecd4a2d20bc68f1b63f399f
onetwo <devbattleratte@web.de>
2012-07-07 18:11:47 +0200

    Implemented the 'controller' in main.cpp. But not tested. Think we should test it on the robots.

--- SoftwareKit/main.cpp
+++ SoftwareKit/main.cpp
@@ -13,6 +13,7 @@
 /* --------------------------- */
 /* ADD INCLUDE TO YOUR PROJECT */
 /* --------------------------- */
+#include <sample.h>
 
 class Project : public AL::ALModule
 {
@@ -27,10 +28,65 @@ public:
 		addParam("param2", "A int");
 		BIND_METHOD(Project::foo);
 		*/
+		functionName("controllerFunction", getName(), "Kick the ball, yeah!");
+		// is it correct to give the param as string?!
+		addParam("kickAngle", "5");
+		BIND_METHOD(Project::controllerFunction);)
 	  }
 
 	  // example function
 	  /*void foo(const float& param1, const int& param2){}*/
+	  void controllerFunction(int kickAngle) {
+		
+		if (kickAngle >= -7 && kickAngle <= 14) {
+			
+			// expecting the DCMProxy::getTime returns time in ms
+			int start_time, current_time;
+			start_time = current_time = DCMProxy::getTime();	  
+		  	counter = 0;
+		  	diff = current_time - start_time;
+		  	
+		  	while (diff < 3100) {
+		  		
+		  		if(diff >= 1000 && counter == 0) {
+		  			
+		  			// statBalance
+		  			counter++;
+		  			Sample::statBalance(true, 500);
+		  			
+		  		} else if (diff >= 1500 && counter = 1){
+		  			
+		  			//raise Foot
+		  			counter++;
+		  			float deltaX;
+		  			
+		  			if (kickAngle <= 0) {
+						deltaX = -kickAngle/(-7.63195750157995)*25;		  					
+		  			} else {
+						deltaX = kickAngle/(14.2093727765404)*25;			  			
+		  			}
+		  			
+		  			Sample::moveFoot(0, deltaX, 30, 500);
+		  			
+		  		} else if (diff >= 2000 && counter = 2) {
+		  			
+		  			//move foot back
+					counter++;  		
+					Sample::moveFoot(-40, 0, 0, 500);
+					
+		  		} else if (diff >= 3000 && counter = 3) {
+		  			
+		  			// kick
+		  			counter++;
+		  			Sample::moveFoot(200, 0, 0, 100);
+		  			
+		  		} 
+		  	
+		  	}
+		  	
+	  	}
+	  	
+	  }
 };
 
 
